90ec506f6d2f33292dbfbb657eb188fc6b6e3456
Elizabeth Pavlova <elizabet.pavlova@estafet.com>
2022-09-21 15:57:52 +0300

    Change hello message from feature branch (N)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Something" );
+        System.out.println( "Hello World! Demo 2" );
     }
 }
